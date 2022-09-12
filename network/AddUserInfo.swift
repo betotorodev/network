c101f2b6c0724be39c267414885264e3105bf1e1
Beto Toro <betotoro0902@gmail.com>
2022-09-12 16:25:21 -0500

    [feat]: integrate the imagePicker

--- network/AddUserInfo.swift
+++ network/AddUserInfo.swift
@@ -11,27 +11,63 @@ struct AddUserInfo: View {
   @Environment(\.dismiss) var dismiss
   @ObservedObject var listOfUser: Users
   @State private var name = ""
+  @State private var image: Image?
+  @State private var inputImage: UIImage?
+  @State private var showingImagePicker = false
   
-    var body: some View {
-      NavigationView {
-        Form {
-          TextField("Name", text: $name)
-        }
-        .navigationTitle("Add a person")
-        .toolbar {
-          Button("Save") {
-            let individualUser = UserInfo(id: UUID(), name: name)
-            listOfUser.users.append(individualUser)
-            dismiss()
+  func loadImage() {
+    guard let inputImage = inputImage else { return }
+    image = Image(uiImage: inputImage)
+  }
+  
+  var body: some View {
+    NavigationView {
+      Form {
+        VStack {
+          HStack {
+            Spacer()
+            ZStack {
+              Rectangle()
+                .fill(.secondary)
+                .frame(width: 200, height: 200)
+              
+              Text("Tap to select a picture")
+                .foregroundColor(.white)
+                .font(.headline)
+              
+              image?
+                .resizable()
+                .scaledToFit()
+            }
+            .onTapGesture {
+              showingImagePicker.toggle()
+            }
+            Spacer()
           }
+          TextField("Name", text: $name)
+            .padding(10)
+        }
+        .padding([.top])
+      }
+      .navigationTitle("Add a person")
+      .toolbar {
+        Button("Save") {
+          let individualUser = UserInfo(id: UUID(), name: name)
+          listOfUser.users.append(individualUser)
+          dismiss()
         }
       }
+      .onChange(of: inputImage) {_ in loadImage()}
+      .sheet(isPresented: $showingImagePicker) {
+        ImagePicker(image: $inputImage)
+      }
     }
+  }
 }
 
 struct AddUserInfo_Previews: PreviewProvider {
-    static var previews: some View {
-      let users = Users()
-      AddUserInfo(listOfUser: users)
-    }
+  static var previews: some View {
+    let users = Users()
+    AddUserInfo(listOfUser: users)
+  }
 }
