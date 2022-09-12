cfb5107f30c6b1d515f3c83e98bf363d86164b71
Beto Toro <beto.toro@zemoga.com>
2022-09-12 10:37:35 -0500

    Initial Commit

---
+++ network/networkApp.swift
@@ -0,0 +1,17 @@
+//
+//  networkApp.swift
+//  network
+//
+//  Created by Beto Toro on 12/09/22.
+//
+
+import SwiftUI
+
+@main
+struct networkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
